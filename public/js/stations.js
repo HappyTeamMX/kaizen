b1893669b04594ef2299fa1d2dafb7f6f31d33da
avathar <a01203437@itesm.mx>
2014-08-29 12:13:52 -0500

    almost all stations done

--- public/js/stations.js
+++ public/js/stations.js
@@ -1,12 +1,38 @@
 $(function () {
     // on load
-    // $('.send-to').hide();
+    $('.send-to').hide();
 
     // bindings
     $('body').on('click','.note',function(){
         $('.note').removeClass('selected');
         $(this).addClass('selected');
+
     });
+
+    $('body').on('click','#start button, #start-normal button',function(){
+      $('#start').slideToggle();
+      $('.send-to').slideToggle();
+    });
+
+    $('body').on('click','.send-to button',function(){
+      var item_id = $('#start input').val();
+      if (item_id != undefined){
+        $('.tickets').append( '<li class="note" data-id=" '+ item_id +' "> '+ item_id+' </li>' );
+      }
+      $('#start input').val('');
+      $('#start').slideToggle();
+      $('.send-to').slideToggle();
+      $('.note.selected').addClass('started');
+
+    });
+
+
+    $('body').on('click','#end button',function(){
+      $('.note.started.selected').hide('swing');
+      $('.note.started.selected').removeClass('selected');
+    });
+
+
 
 
 });
